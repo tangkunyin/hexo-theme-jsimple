5eed84ede1acfaf8e13b31d3e548e19272332cd2
tangkunyin <iam@tangkunyin.com>
2017-03-05 12:21:45 +0800

    update donate title

--- source/js/SimpleCore.js
+++ source/js/SimpleCore.js
@@ -56,7 +56,7 @@ var SimpleCore = {
         $(document).on('click', '.btn-donate', function (e) {
             e.preventDefault();
             if (SimpleCore.donateImg != '') {
-                SimpleCore.alert('小奖赏激发大动力','<img style="width:160px;background:#fff;" src="' + SimpleCore.donateImg + '">');
+                SimpleCore.alert('小奖赏激发大创造','<img style="width:160px;background:#fff;" src="' + SimpleCore.donateImg + '">');
             } else {
                 SimpleCore.alert('暂未开通打赏功能','<h4 style="text-align: center;margin: 0">联系博主试试看 ：）</h4>');
             }
