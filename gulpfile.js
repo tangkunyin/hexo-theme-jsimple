42cdfdaf148a693eb122b772de4a23c0f498716d
Thomas Tang <iam@tangkunyin.com>
2021-01-12 11:52:33 +0800

    :bug: (gulp): fix babel ignore libs

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,36 @@
 'use strict';
 
-const babel = require ('gulp-babel');
 const gulp = require ('gulp');
+const babel = require ('gulp-babel');
 const minifycss = require ('gulp-clean-css');
 const uglify = require ('gulp-uglify');
 const rename = require ('gulp-rename');
 const concat = require ('gulp-concat');
 
+const srcIgnore = [
+  './source/js/lib/zepto.min.js',
+  './source/js/lib/busuanzi.pure.js',
+  './source/js/lib/md5.js',
+];
+gulp.task ('minify-js', async function () {
+  await gulp
+    .src (srcIgnore.concat('./source/js/lib/SimpleCore.js'))
+    .pipe (
+      babel ({
+        ignore: srcIgnore,
+        presets: ['@babel/preset-env']
+      })
+    )
+    .pipe (uglify ())
+    .pipe (concat ('./source/js/SimpleCore.min.js'))
+    .pipe (gulp.dest ('./'));
+});
 gulp.task ('minify-css', async function () {
   await gulp
     .src ('./source/css/JSimple.css')
     .pipe (minifycss ())
     .pipe (rename ({extname: '.min.css'}))
     .pipe (gulp.dest ('./source/css'));
-});
-gulp.task ('minify-js', async function () {
-  await gulp
-    .src ([
-      './source/js/lib/zepto.min.js',
-      './source/js/lib/busuanzi.pure.js',
-      './source/js/lib/md5.js',
-      './source/js/lib/SimpleCore.js',
-    ])
-    .pipe (
-      babel ({
-        presets: ['@babel/preset-env'],
-      })
-    )
-    .pipe (uglify ())
-    .pipe (concat ('./source/js/SimpleCore.min.js', {newLine: ';'}))
-    .pipe (gulp.dest ('./'));
 });
 
 process.on ('unhandledRejection', error => {
@@ -39,7 +40,7 @@ process.on ('unhandledRejection', error => {
 
 gulp.task (
   'default',
-  gulp.parallel ('minify-css', 'minify-js', function (done) {
+  gulp.parallel ('minify-js', 'minify-css', function (done) {
     done ();
   })
 );
