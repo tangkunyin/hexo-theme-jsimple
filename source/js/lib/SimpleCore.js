e138738a63dfc7534a3a642f94972bec1f459634
Thomas Tang <iam@tangkunyin.com>
2021-01-27 20:40:17 +0800

    :bug: (Corejs): fix Zepto ScrollTo error

--- source/js/lib/SimpleCore.js
+++ source/js/lib/SimpleCore.js
@@ -1,4 +1,40 @@
 /**
+ * Zepto扩展
+ */
+$.fn.scrollTo = function(options) {
+        const defaults = {
+            toT : 0,    //滚动目标位置
+            durTime : 500,  //过渡动画时间
+            delay : 30,     //定时器时间
+            callback:null   //回调函数
+        };
+        let opts = $.extend(defaults,options),
+            timer = null,
+            _this = this,
+            curTop = _this.scrollTop(),//滚动条当前的位置
+            subTop = opts.toT - curTop,    //滚动条目标位置和当前位置的差值
+            index = 0,
+            dur = Math.round(opts.durTime / opts.delay),
+            smoothScroll = function(t){
+                index++;
+                let per = Math.round(subTop/dur);
+                if(index >= dur) {
+                    _this.scrollTop(t);
+                    window.clearInterval(timer);
+                    if(opts.callback && typeof opts.callback == 'function'){
+                        opts.callback();
+                    }
+                    return;
+                } else {
+                    _this.scrollTop(curTop + index*per);
+                }
+            };
+        timer = window.setInterval(function(){
+            smoothScroll(opts.toT);
+        }, opts.delay);
+        return _this;
+    };
+/**
  * Local Search
  * @author https://github.com/theme-next/hexo-theme-next
  */
@@ -400,7 +436,7 @@ const SimpleCore = {
         SimpleCore.setDefaultReadingMode();
     },
     goTop () {
-        $("html, body").animate({scrollTop: 0}, 200);
+        $("html, body").scrollTo({toT: 0});
     },
     setPageCurrent () {
         if (SimpleCore.isPost === 'true') {
